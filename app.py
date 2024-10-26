3c43523c04b4d671f2a2b018df762f64f15357f8
Muhammad Akhta <akhtacaesar@gmail.com>
2024-10-26 12:03:21 +0700

    Initial Template

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, redirect, request, url_for
+
+app = Flask(__name__)
+
+# Sample data for classrooms
+classrooms = [
+    {"name": "Math 101", "yearlevel": "Freshman", "capacity": 30},
+    {"name": "Science 102", "yearlevel": "Sophomore", "capacity": 25},
+    {"name": "History 201", "yearlevel": "Junior", "capacity": 20},
+    {"name": "English 202", "yearlevel": "Senior", "capacity": 28}
+]
+
+@app.route("/")
+def home():
+  data = "Test sentence written in app.py"
+  return render_template("index.html", data=data)
+
+@app.route("/create", methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        name = request.form['name']
+        yearlevel = request.form['yearlevel']
+        capacity = request.form['capacity']
+        classrooms.append({"name":name, "yearlevel":yearlevel, "capacity":capacity})
+        return redirect(url_for("view"))
+    return render_template('create.html')
+
+@app.route('/view')
+def view():
+    return render_template('view.html', classrooms = classrooms)
+
+@app.route('/update')
+def update():
+    return render_template('update.html')
+
+# Running the app in development mode
+if __name__ == "__main__":
+  app.run(host="0.0.0.0",debug=True)
